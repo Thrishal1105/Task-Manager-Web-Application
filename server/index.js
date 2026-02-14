d1ae920a413dc1f40048aa8d1e42ea13f33bbd50
Thrishal1105 <dharshanojthrishal11@gmail.com>
2026-02-15 00:26:14 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -21,6 +21,9 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
